d988d532d0deb1599e305834db40cd7a602421ae
Mayur Balu Dabade <mayurdabade1103@gmail.com>
2024-05-06 08:54:20 +0530

    updated app.py

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 import streamlit as st
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image as keras_image
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image
 import numpy as np
 from PIL import Image
+import matplotlib.pyplot as plt
 
 # Load the pre-trained model
-model = load_model('my_pneumonia_detection_model.h5')
-img_width, img_height = 256, 256
-
-# from tensorflow.keras.preprocessing import image as keras_image
+model = tf.keras.models.load_model("my_pneumonia_detection_model.h5")
+img_width, img_height = 224, 224  # Update the target size to match the model input shape
 
 def preprocess_image(image):
     # Convert image to RGB if it has fewer than 3 channels
@@ -17,7 +16,7 @@ def preprocess_image(image):
     # Resize image to match model input shape
     image = image.resize((img_width, img_height))
     # Convert image to numpy array
-    image = keras_image.img_to_array(image)
+    image = np.array(image)
     # Ensure the image has 3 color channels (RGB)
     if image.shape[-1] != 3:
         raise ValueError("Image does not have 3 color channels (RGB)")
@@ -27,6 +26,16 @@ def preprocess_image(image):
     image = image / 255.0
     return image
 
+def image_prediction(new_image_path):
+    test_image = Image.open(new_image_path)
+    test_image = preprocess_image(test_image)
+    prediction = model.predict(test_image)
+    plt.imshow(test_image.squeeze())  # Plot the image
+    plt.axis('off')  # Turn off axis
+    if prediction[0] > 0.5:
+        st.error('Affected  by Pneumonia', icon="🚨")
+    else:
+        st.success('Result is NORMAL !!', icon="😊")
 
 # Streamlit app
 st.title('🌡️🦠Pneumonia Detection Model')
@@ -36,20 +45,5 @@ uploaded_file = st.file_uploader("Choose chest X-ray image...", type=["jpg", "jp
 if uploaded_file is not None:
     # Display uploaded image
     image = Image.open(uploaded_file)
-    st.image(image,width=400)
-
-    # Preprocess the image
-    processed_image = preprocess_image(image)
-
-    # Make prediction
-    prediction = model.predict(processed_image)
-
-    # Get class label
-    class_label = np.argmax(prediction)
-    
-    # Display prediction
-    if class_label==1:
-        st.error('Affected  by Pneumonia', icon="🚨")
-    else:
-        st.success('Result is NORMAL !!', icon="😊")
-       
+    st.image(image, width=400)
+    image_prediction(uploaded_file)
